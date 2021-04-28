d821811d02c7aa0e8cbdacc027d988b28d707f96
karuvsentor <karuvsentor@gmail.com>
2021-04-28 20:11:09 +0200

    Google account funciona y guarda en BBDD, pero no me deja entrar en userprofile

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,18 +4,18 @@ const eventController = require("../controllers/event.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 
 //all events
-router.get("/", eventController.getAllfromDB);
+router.get("/",authMiddleware.isAuthenticated, eventController.getAllfromDB);
 
 //create
-router.post("/create", eventController.create);
+router.post("/create",authMiddleware.isAuthenticated, eventController.create);
 
 //get
-router.get("/:id", eventController.get);
+router.get("/:id",authMiddleware.isAuthenticated, eventController.get);
 
 //delete
-router.post("/delete/:id", eventController.delete);
+router.post("/delete/:id",authMiddleware.isAuthenticated, eventController.delete);
 
 //edit event
-router.put("/edit/:id", authMiddleware.isAuthenticated, eventController.edit);
+router.put("/edit/:id",authMiddleware.isAuthenticated, authMiddleware.isAuthenticated, eventController.edit);
 
 module.exports = router;
